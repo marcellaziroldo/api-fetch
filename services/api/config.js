78bf3d65ca99e853afb9136c2f7d8a560d0a6d63
Marcella <marcella.mziroldo@gmail.com>
2025-03-17 20:53:12 -0600

    in class exercise

---
+++ services/api/config.js
@@ -0,0 +1,10 @@
+    export const API_CONFIG_OPEN_FOOD = {
+        BASE_URL: "https://world.openfoodfacts.org/api/v0/product/3017620422003.json",
+        HEADERS: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      };
+  
+
+  
